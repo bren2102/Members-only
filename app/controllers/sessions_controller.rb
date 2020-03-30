3aee69d63eca815b2e88a83ab7e56a326a1e17cc
xtrmdarc <diegoreyesco@hotmail.com>
2020-03-30 18:00:25 -0500

    Add login form

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,8 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def login_user
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/signin',     to:'sessions#new'
+  post '/login',     to:'sessions#login_user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
