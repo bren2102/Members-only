0f14cedbedfe24f9e0bd0912e2bcaf1d11828018
xtrmdarc <diegoreyesco@hotmail.com>
2020-04-01 12:01:01 -0500

    Create Sign up functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:welcome] = 'Welcome to the Clubhouse'
+      log_in_user(@user)
+      remember(@user)
+      redirect_to posts_path
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require('user').permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   get '/login',     to:'sessions#new'
   post '/login',     to:'sessions#create'
   delete '/logout',  to:'sessions#delete'
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:new, :create, :index]
+  resources :users, only: [:new, :create]
 end
