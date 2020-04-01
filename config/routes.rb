95273e1cef0144d5adc10f68aa228e8a8a47d2d1
xtrmdarc <diegoreyesco@hotmail.com>
2020-04-01 11:19:24 -0500

    Update login routes and format README.md

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get '/signin',     to:'sessions#new'
+  get '/login',     to:'sessions#new'
   post '/login',     to:'sessions#create'
-  get '/feed',       to:'application#index'
   delete '/logout',  to:'sessions#delete'
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
